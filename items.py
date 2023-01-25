ae310a7887b15b12262dc1bf4ab81495a883d55c
uwagaDUDE <puvir00@gmail.com>
2023-01-25 16:34:57 +0300

    Update items lits, added items ids list

--- items.py
+++ items.py
@@ -49,54 +49,28 @@ class Items:
                 self.heal = 5
                 self.sell_price = 0
                 self.buy_price = 5
-#     def __init__(self, name, damage, armor, sell_cost, buy_cost, id):
-#         self.name = name
-#         self.damage = damage
-#         self.armor = armor
-#         self.sell_cost = sell_cost
-#         self.buy_cost = buy_cost
-#         self.id = id
-# class Item:
-#     # def wooden_sword(self):
-#     #     return Items(name='Деревяннй меч',
-#     #                  damage=random.randint(5,8),
-#     #                  sell_cost=5,
-#     #                  buy_cost=10,
-#     #                  id=0)
-#
-#     def wooden_sword(self):
-#         return Items('Деревяннй меч',
-#                      random.randint(5,8),
-#                      5,
-#                      10,
-#                      0)
+                self.item_id = 300
 
-    #def wooden_sword(self, name='Деревяннй меч', damage=random.randint(5,8), sell_cost=5, buy_cost=10, id=0):
-        #return name, damage, sell_cost, buy_cost, id
+        class GrilledChicken:
 
+            def __init__(self):
+                self.name = 'Жареная курица'
+                self.heal = 50
+                self.sell_price = 0
+                self.buy_price = 40
+                self.item_id = 301
+    class QuestItems:
 
+        class TestItem:
+            def __init__(self):
+                self.name = 'Ветка'
+                self.item_id = 500
 
-# def get_random_item(list):
-#     return random.choice(list)
-#
-#
-# spawn_items = [wooden_sword, gowno]
-#
-#
-# class Player:
-#     def __init__(self):
-#         self.hp = 100
-#         self.items = []
-#         self.damage = 0
-#
-#     def calc_damage(self):
-#         calculated_value = 0;
-#         for item in self.items:
-#             calculated_value += item.damage
-#         self.damage = calculated_value
-#
-#
-# player = Player()
-# player.items.append(get_random_item(spawn_items))
-# player.calc_damage()
-# print(player.damage)
+    class CraftItem:
+
+        class RawChicken:
+
+            def __init__(self):
+                self.name = 'Сырая курица'
+                self.buy_price = 10
+                self.item_id = 1000
