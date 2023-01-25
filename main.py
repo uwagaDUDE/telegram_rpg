b40c92b9581bbd861c313dbd014e0fa81b3e60f1
Roland <machodin@yandex.ru>
2023-01-25 21:54:01 +0300

    added tg integration and mongodb connect

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import interface
-import items, player
-# import postSql
+import items
+import player
+import db
 import scripts
+import telebot
+import os
 weapon = items.Items().Weapons()
-name = interface.StartGame().start_game(weapon.WoodenSword().name, 2)
-player = player.Player(name)
+# name = interface.StartGame().start_game(weapon.WoodenSword().name, 2)
+# player = player.Player(name)
 move = scripts.Movement()
 interface = interface.Interface()
+bot = telebot.TeleBot("5855247722:AAGB3OF41dXmqqZQndYsuux5-2iIsxvP348")
 
-def main():
+@bot.message_handler(commands=['start'])  # /start - Главное меню
+def handle_start(message):
+    user = message.from_user
+    player = db.getPlayer(user.id)
+    if player == None:
+        db.createPlayer(user.id)
+        player = db.getPlayer(user.id)
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('БЛОК1', 'БЛОК2')
+    user_markup.row('БЛОК3', 'БЛОК4')
+    print(player)
+    bot.send_message(user.id, f'Добро пожаловать {user.first_name} \nВаш персонаж: \n   Уровень: {player}', reply_markup=user_markup)
 
-    movement = True
-    while movement == True:
-
-        interface.what_to_do()
-        user_choice = input('\nЧто будем делать: ')
-
-        if user_choice == "1":
-            move.forward_move()
-        elif user_choice == "2":
-            move.left_move()
-        elif user_choice == "3":
-            move.right_move()
-        else:
-            print('NOT REALISED')
-
-
-main()
+bot.polling(none_stop=True, interval=0)
