e0c1bb001658b924000dff972a6046237d8b67fe
Roland <machodin@yandex.ru>
2023-01-26 16:23:14 +0300

    fix bot

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import player
 import db
 import scripts
 import telebot
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 weapon = items.Items().Weapons()
 # name = interface.StartGame().start_game(weapon.WoodenSword().name, 2)
 # player = player.Player(name)
 move = scripts.Movement()
 interface = interface.Interface()
-bot = telebot.TeleBot("5855247722:AAGB3OF41dXmqqZQndYsuux5-2iIsxvP348")
+bot = telebot.TeleBot(os.environ.get('API_KEY'))
 
 event = scripts.Event()
+
 
 @bot.message_handler(commands=['start'])  # /start - Главное меню
 def handle_start(message):
@@ -22,7 +26,10 @@ def handle_start(message):
         db.createPlayer(user.id)
         player = db.getPlayer(user.id)
     user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
-    user_markup.row('БЛОК1', 'БЛОК2')
+    user_markup.row('Начать путешестиве', 'Посмотреть инвентарь')
     user_markup.row('БЛОК3', 'БЛОК4')
-    print(player)
-    bot.send_message(user.id, f'Добро пожаловать {user.first_name} \nВаш персонаж: \n   Уровень: {player}', reply_markup=user_markup)
+    bot.send_message(
+        user.id, f'Добро пожаловать {user.first_name} \nВаш персонаж: \n   Уровень: { player["level"] }   \n Предметов в инвентаре: {len(player["items"])}', reply_markup=user_markup)
+
+
+bot.polling(none_stop=True, interval=0)
