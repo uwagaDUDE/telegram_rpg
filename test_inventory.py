fcc9b5392c0a12dc799180671dd86e594ad04029
uwagaDUDE <puvir00@gmail.com>
2023-01-28 11:19:57 +0300

    inv syst

--- test_inventory.py
+++ test_inventory.py
@@ -4,7 +4,12 @@ items = \
     'healing_flask':0,
     'iron_sword':0
     }
-
+player = {
+    'items':'',
+    'test':''
+}
+item_movement = []
+second_item_movement = []
 print(f'Взять деревянный меч в руки - 1, взять железный - 2, выбросить - 3, оружие в руке - 4')
 def pick_up_sword():
     stop = False
@@ -40,5 +45,41 @@ def pick_up_sword():
                     items[a] = 0
             weapon_in_hand.clear()
 
+class Pizdos:
+    #ДОБАВЛЯЕМ НАЗВАНИЕ ПРЕДМЕТА В СПИСОК
+    def add_item_in_inventory(self):
+       return item_movement.append('Test')
+    #ИЗ СПИСКА ДЕЛАЕМ СТРОКУ, ХЗ ЗАЧЕМ ЕСЛИ МОЖНО БЫЛО СРАЗУ В СТРОКУ ДОБАВЛЯТЬ А НЕ В СПИСОК
+    def into_str(self):
+        stroka = ''
+        for i in item_movement:
+            stroka += i
+        return stroka
+    #ТО ЖЕ САМОЕ ПРОДЕЛЫВАЕМ С КОЛИЧЕСТВОМ
+    def second_add_item_in_inventory(self):
+        return second_item_movement.append(1)
 
-pick_up_sword()
+    def sec_into_str(self):
+        stroka = 0
+        for i in second_item_movement:
+            stroka += i
+        return stroka
+    #ЗАБРАСЫВАЕМ В ИНВЕНТАРЬ
+    def back_to_dict(self):
+        global player
+        player['items'] = {self.into_str():self.sec_into_str()}
+        return player['items']
+    #GUI
+    def put_in_inv(self):
+        print('Полож на место и не трогай!')
+        return input('Взять (нажми 1): ')
+    #ПОРЯДОК ВЫПОЛНЕНИЯ
+    def print_dict(self):
+        if self.put_in_inv() == "1":
+            self.add_item_in_inventory()
+            self.second_add_item_in_inventory()
+            print(self.back_to_dict())
+#ВЫВОДИМ
+test = Pizdos()
+test.print_dict()
+
